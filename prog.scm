965679ca253b4c56470a6ba0ba27be9de1ddde06
Hunter McCallister <huntermccalliste@u.boisestate.edu>
2025-09-16 13:14:58 -0600

    Scaffold

--- prog.scm
+++ prog.scm
@@ -1,3 +1,16 @@
-; (super-duper)
-(display "Hello, World!")
-(newline)
+;; CS354 Language Assignment #1
+;; Author: Hunter McCallister
+;; Description: 
+
+;; --- Main function ---
+(define (super-duper source count)
+  ;; return source unchanged
+  source)
+
+;; --- Testers ---
+
+(display "Testing...") (newline)
+
+(display (super-duper 123 1)) (newline)   ; 123
+(display (super-duper '() 1)) (newline)   ; ()
+(display (super-duper '(x y) 1)) (newline); (x y)
