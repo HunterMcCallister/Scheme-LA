b893b8c0139869c4ff5851943d590b7c42f19717
huntermccallister <huntermccallister@gmail.com>
2025-09-16 17:27:16 -0600

    passing tests

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-Place holder
+# Project: Super-Duper (Scheme Recursion)  
+**Author:** Hunter McCallister  
+**Class:** CS354  
+ 
+
+---
+
+## Overview  
+This project implements the Scheme function **`super-duper`**, which duplicates elements of a list a specified number of times. If the input is an atom, it is returned unchanged. If the input is a list, the program recursively walks through the list, duplicates each element `count` times, and returns a new list.  
+
+Two helper functions support this:  
+- **`replicate`**: creates a list with `n` copies of a given element.  
+- **`cat`**: concatenates two lists using only `cons`.  
+
+The function demonstrates recursive thinking in functional programming, without loops or mutation, and uses only basic Scheme primitives (`cons`, `car`, `cdr`, `pair?`, `null?`).  
+
+---
+
+## Compiling and Using  
+To run the program:  
+1. Make sure Guile Scheme is installed.  
+2. Save the code into a file named `prog.scm`
+3. Run in the terminal: 
+`guile prog.scm`
+
+
+
+---
+
+## Results  
+Running the provided tests displays the expected outputs:  
+- `(super-duper 123 2)` → `123`  
+- `(super-duper '(x y) 2)` → `(x x y y)`  
+- `(super-duper '((a b) y) 3)` → `((a a a b b b) (a a a b b b) (a a a b b b) y y y)`  
+
+The program correctly handles atoms, empty lists, simple lists and nested lists.  
--- prog.scm
+++ prog.scm
@@ -1,16 +1,49 @@
 ;; CS354 Language Assignment #1
 ;; Author: Hunter McCallister
-;; Description: 
+;; Description: uses recursive function super-duper that takes a list and a count adn returns
+;; a new list with every element duplicated count times. Uses helper methods to replicate and concatenation.
 
 ;; --- Main function ---
 (define (super-duper source count)
-  ;; return source unchanged
-  source)
+  (if (not (pair? source)) ;;If source is not a pair
+    source ;;returns source as is
+    (cat (replicate (super-duper (car source) count) count) ;; left arg for cat
+          (super-duper (cdr source) count)))) ;; right arg
+
+
+;; Helper methods
+
+;;--- replicate ---
+;; Returns a list with n copies of x
+(define (replicate x n)
+  (if (= n 0)
+    '() ;;base case
+    (cons x (replicate x (- n 1)))) ;;recursion on x
+)
+
+;; --- concat ---
+;; Concatenate two lists 
+(define (cat xs ys)
+  (if (null? xs)
+    ys
+    (cons (car xs) (cat (cdr xs) ys)))) ;; strip one element from xs and build new list
+
 
 ;; --- Testers ---
 
-(display "Testing...") (newline)
+(display "Running Testers...") (newline)
 
-(display (super-duper 123 1)) (newline)   ; 123
-(display (super-duper '() 1)) (newline)   ; ()
-(display (super-duper '(x y) 1)) (newline); (x y)
+;; Assignment examples 
+(display (super-duper 123 1)) (newline)        ; => 123
+(display (super-duper 123 2)) (newline)        ; => 123
+(display (super-duper '() 1)) (newline)        ; => ()
+(display (super-duper '() 2)) (newline)        ; => ()
+(display (super-duper '(x) 1)) (newline)       ; => (x)
+(display (super-duper '(x) 2)) (newline)       ; => (x x)
+(display (super-duper '(x y) 1)) (newline)     ; => (x y)
+(display (super-duper '(x y) 2)) (newline)     ; => (x x y y)
+(display (super-duper '((a b) y) 3)) (newline) ; => ((a a a b b b) (a a a b b b) (a a a b b b) y y y)
+;; extra 
+(display (super-duper '(x y) 0)) (newline)     ; reasonable => ()
+(display (super-duper '((a (b)) c) 2)) (newline)
+(display (super-duper 'x 0)) (newline)         ; => x
